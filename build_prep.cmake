f34d8488996acd2341a30c8333592f0a0b1cc05a
Jindrich Luza <jluza@redhat.com>
2013-06-25 16:38:45 +0200

    libcomps.spec made github compatible

--- build_prep.cmake
+++ build_prep.cmake
@@ -3,12 +3,12 @@ include (${CMAKE_SOURCE_DIR}/libcomps/version.cmake)
 set(VERSION ${libcomps_VERSION_MAJOR}.${libcomps_VERSION_MINOR}.${libcomps_VERSION_PATCH})
 
 exec_program("git" ARGS rev-parse --short HEAD OUTPUT_VARIABLE GITREV)
-#exec_program("git" ARGS rev-parse HEAD OUTPUT_VARIABLE GITREVLONG)
+exec_program("git" ARGS rev-parse HEAD OUTPUT_VARIABLE GITREVLONG)
 
 
-set(GITARG archive ${GITREV} "--format=tar.gz" "--prefix=libcomps/")
+set(GITARG archive ${GITREV} "--format=zip" "--prefix=libcomps-${GITREVLONG}/")
 set(GITCMD "git")
-execute_process(COMMAND ${GITCMD} ${GITARG} OUTPUT_FILE "libcomps-${GITREV}.tar.xz")
+execute_process(COMMAND ${GITCMD} ${GITARG} OUTPUT_FILE "libcomps-${GITREVLONG}.tar.gz")
 
 configure_file(libcomps.spec.in libcomps.spec)
 
