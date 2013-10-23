41e5eedc3cdde7431263cd0ab1d2f8e144e3da07
Jindrich Luza <jluza@redhat.com>
2013-10-23 12:37:07 +0200

    - rewriting documentation

--- libcomps/src/comps_doc.h
+++ libcomps/src/comps_doc.h
@@ -30,6 +30,12 @@
 #include "comps_doccategory.h"
 #include "comps_docenv.h"
 
+/** \file comps_doc.h
+ * \brief COMPS_Doc header file
+ * @see COMPS_Doc_getters @see COMPS_Doc_setters
+ */
+
+/** @cond NOTMET */
 #define COMPS_DOC_GETOBJLIST(OBJS) COMPS_ObjList* CONCAT(comps_doc_, OBJS)\
                                                            (COMPS_Doc *doc){\
     COMPS_ObjList *ret;\
@@ -41,17 +47,22 @@
     }\
     return ret;\
 }
+/** <@hideinititalizer */
+
 #define HEAD_COMPS_DOC_GETOBJLIST(OBJS) COMPS_ObjList* CONCAT(comps_doc_, OBJS)\
                                                            (COMPS_Doc *doc);
+/** <@hideinititalizer */
 
 #define COMPS_DOC_SETOBJLIST(OBJS) void CONCAT(comps_doc_set_, OBJS)\
                                                        (COMPS_Doc *doc,\
                                                         COMPS_ObjList *list){\
     comps_objdict_set(doc->objects, #OBJS, (COMPS_Object*)list);\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_SETOBJLIST(OBJS) void CONCAT(comps_doc_set_, OBJS)\
                                                    (COMPS_Doc *doc,\
                                                     COMPS_ObjList *list);
+/** <@hideinititalizer */
 
 #define COMPS_DOC_GETOBJDICT(OBJNAME) COMPS_ObjDict* CONCAT(comps_doc_, OBJNAME)\
                                                            (COMPS_Doc *doc){\
@@ -64,8 +75,10 @@
     }\
     return ret;\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_GETOBJDICT(OBJNAME) COMPS_ObjDict* CONCAT(comps_doc_, OBJNAME)\
                                                            (COMPS_Doc *doc);
+/** <@hideinititalizer */
 
 #define COMPS_DOC_GETOBJMDICT(OBJNAME) COMPS_ObjMDict* CONCAT(comps_doc_, OBJNAME)\
                                                            (COMPS_Doc *doc){\
@@ -78,26 +91,32 @@
     }\
     return ret;\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_GETOBJMDICT(OBJNAME) COMPS_ObjMDict* CONCAT(comps_doc_, OBJNAME)\
                                                            (COMPS_Doc *doc);
+/** <@hideinititalizer */
 
 #define COMPS_DOC_SETOBJDICT(OBJS) void CONCAT(comps_doc_set_, OBJS)\
                                                        (COMPS_Doc *doc,\
                                                         COMPS_ObjDict *dict){\
     comps_objdict_set(doc->objects, #OBJS, (COMPS_Object*)dict);\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_SETOBJDICT(OBJS) void CONCAT(comps_doc_set_, OBJS)\
                                                    (COMPS_Doc *doc,\
                                                     COMPS_ObjDict *dict);
+/** <@hideinititalizer */
 
 #define COMPS_DOC_SETOBJMDICT(OBJS) void CONCAT(comps_doc_set_, OBJS)\
                                                        (COMPS_Doc *doc,\
                                                         COMPS_ObjMDict *dict){\
     comps_objdict_set(doc->objects, #OBJS, (COMPS_Object*)dict);\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_SETOBJMDICT(OBJS) void CONCAT(comps_doc_set_, OBJS)\
                                                    (COMPS_Doc *doc,\
                                                     COMPS_ObjMDict *dict);
+/** <@hideinititalizer */
 
 
 #define COMPS_DOC_ADDOBJLIST(OBJS, OBJNAME, OBJTYPE) void CONCAT(comps_doc_add_,\
@@ -114,10 +133,12 @@
     COMPS_OBJECT_DESTROY(ret);\
     comps_objlist_append_x(ret, (COMPS_Object*)obj);\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_ADDOBJLIST(OBJNAME, OBJTYPE) void CONCAT(comps_doc_add_,\
                                                            OBJNAME)\
                                                            (COMPS_Doc *doc,\
                                                             OBJTYPE *obj);
+/** <@hideinititalizer */
 
 #define COMPS_DOC_ADDOBJDICT(OBJS, OBJNAME) void CONCAT(comps_doc_add_,\
                                                            OBJNAME)\
@@ -133,11 +154,13 @@
     comps_objdict_set_x(ret, key, (COMPS_Object*)obj);\
     COMPS_OBJECT_DESTROY(ret);\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_ADDOBJDICT(OBJNAME) void CONCAT(comps_doc_add_,\
                                                            OBJNAME)\
                                                            (COMPS_Doc *doc,\
                                                             char *key,\
                                                             COMPS_Str *obj);
+/** <@hideinititalizer */
 
 
 #define COMPS_DOC_ADDOBJMDICT(OBJS, OBJNAME) void CONCAT(comps_doc_add_,\
@@ -154,54 +177,216 @@
     comps_objmdict_set_x(ret, key, (COMPS_Object*)obj);\
     COMPS_OBJECT_DESTROY(ret);\
 }
+/** <@hideinititalizer */
 #define HEAD_COMPS_DOC_ADDOBJMDICT(OBJNAME) void CONCAT(comps_doc_add_,\
                                                            OBJNAME)\
                                                            (COMPS_Doc *doc,\
                                                             char *key,\
                                                             COMPS_Str *obj);
+/** <@hideinititalizer */
+/** @endcond*/
 
+
+/** COMPS_Object derivate containing whole comps.xml document.
+ */
 typedef struct {
     COMPS_Object_HEAD;
-    COMPS_ObjDict *objects;
+    COMPS_ObjDict *objects; /**< dictionary of comps subobjects */
     COMPS_Log *log;
-    COMPS_Str *encoding;
+    /**< COMPS_Log object to store log messages evoked
+     * by parsing and xml generating */
+    COMPS_Str *encoding;   /**< comps.xml document encoding */
     } COMPS_Doc;
 
 //HEAD_COMPS_CREATE_u(doc, COMPS_Doc)  /*comps_utils.h macro*/
 //HEAD_COMPS_COPY_u(doc, COMPS_Doc)  /*comps_utils.h macro*/
 //HEAD_COMPS_DESTROY_u(doc, COMPS_Doc)  /*comps_utils.h macro*/
 
+/** constructor callback for COMPS_Doc object. COMPS_Doc is COMPS_Object
+ * derivate. Use comps_object_create() for construction instead
+ *
+ * @param doc allocated COMPS_Doc object
+ * @param args array of constructor arguments. COMPS_Doc constructor accepts
+ * encoding argument as COMPS_Str object only so array need not end with
+ * sentinel item*/
 void comps_doc_create(COMPS_Doc* doc, COMPS_Object **args);
+
+/** copy callback for COMPS_Doc object*/
 void comps_doc_copy(COMPS_Doc *doc_dst, COMPS_Doc *doc_src);
+
+/** destructor callback for COMPS_Doc object. COMPS_Doc is COMPS_Object
+ * derivate. Use comps_object_create() for construction instead*/
 void comps_doc_destroy(COMPS_Doc *doc);
+
+/** comparator callback for COMPS_Doc object */
 signed char comps_doc_cmp_u(COMPS_Object *obj1, COMPS_Object *obj2);
 
+/** \defgroup COMPS_Doc_getters COMPS_Doc getters
+ * @{
+ */
+
+/** comps group list getter
+ * @param doc COMPS_Doc instance
+ * @return COMPS_ObjList object of COMPS_DocGroup*/
 HEAD_COMPS_DOC_GETOBJLIST(groups) /*comps_doc.h macro*/
+
+/** comps category list getter
+ * @param doc COMPS_Doc instance
+ * @return COMPS_ObjList object of COMPS_DocCategory*/
 HEAD_COMPS_DOC_GETOBJLIST(categories) /*comps_doc.h macro*/
+
+/** comps environment list getter
+ * @param doc COMPS_Doc instance
+ * @return COMPS_ObjList object of COMPS_DocEnv*/
 HEAD_COMPS_DOC_GETOBJLIST(environments) /*comps_doc.h macro*/
+
+/** comps langpack dictionary getter
+ * @param doc COMPS_Doc instance
+ * @return COMPS_ObjDict object of COMPS_Str*/
 HEAD_COMPS_DOC_GETOBJDICT(langpacks) /*comps_doc.h macro*/
+
+/** comps blacklist dictionary getter
+ * @param doc COMPS_Doc instance
+ * @return COMPS_ObjMDict object of COMPS_Str*/
 HEAD_COMPS_DOC_GETOBJMDICT(blacklist) /*comps_doc.h macro*/
+
+/** comps whiteout dictionary getter
+ * @param doc COMPS_Doc instance
+ * @return COMPS_ObjMDict object of COMPS_Str*/
 HEAD_COMPS_DOC_GETOBJMDICT(whiteout) /*comps_doc.h macro*/
 
+/**@}*/
+
+/** \defgroup COMPS_Doc_setters COMPS_Doc setters
+ * @{
+ */
+
+/** comps group list setter
+ * @param doc COMPS_Doc instance
+ * @param list COMPS_ObjList of COMPS_DocGroup items
+ * \warning make sure of correct items type. Setter doesn't provide any
+ * additional control routines
+ */
 HEAD_COMPS_DOC_SETOBJLIST(groups) /*comps_doc.h macro*/
+
+/** comps category list setter
+ * @param doc COMPS_Doc instance
+ * @param list COMPS_ObjList of COMPS_DocCategory items
+ * \warning make sure of correct items type. Setter doesn't provide any
+ * additional control routines
+ */
 HEAD_COMPS_DOC_SETOBJLIST(categories) /*comps_doc.h macro*/
+
+/** comps environments list setter
+ * @param doc COMPS_Doc instance
+ * @param list COMPS_ObjList of COMPS_DocEnv items
+ * \warning make sure of correct items type. Setter doesn't provide any
+ * additional control routines
+ */
 HEAD_COMPS_DOC_SETOBJLIST(environments) /*comps_doc.h macro*/
+
+/** comps lankpack dict setter
+ * @param doc COMPS_Doc instance
+ * @param dict COMPS_ObjDict of COMPS_Str items
+ * \warning make sure of correct items type. Setter doesn't provide any
+ * additional control routines
+ */
 HEAD_COMPS_DOC_SETOBJDICT(langpacks) /*comps_doc.h macro*/
+
+/** comps blacklist multi-dict setter
+ * @param doc COMPS_Doc instance
+ * @param dict COMPS_ObjMDict of COMPS_Str items
+ * \warning make sure of correct items type. Setter doesn't provide any
+ * additional control routines
+ */
 HEAD_COMPS_DOC_SETOBJMDICT(blacklist) /*comps_doc.h macro*/
+
+/** comps whiteout multi-dict setter
+ * @param doc COMPS_Doc instance
+ * @param dict COMPS_ObjMDict of COMPS_Str items
+ * \warning make sure of correct items type. Setter doesn't provide any
+ * additional control routines
+ */
 HEAD_COMPS_DOC_SETOBJMDICT(whiteout) /*comps_doc.h macro*/
 
+/**@}*/
+
+/** \defgroup COMPS_Doc_adders COMPS_Doc adders
+ * @{
+ */
+
+/** COMPS_DocGroup adder to group list in COMPS_Doc
+ * @param doc COMPS_Doc instance
+ * @param obj COMPS_DocGroup object
+ * append COMPS_DocGroup object to group list in COMPS_Doc structure
+ * \warning function doesn't increment COMPS_DocGroup object reference count.
+ */
 HEAD_COMPS_DOC_ADDOBJLIST(group, COMPS_DocGroup) /*comps_doc.h macro*/
+
+/** COMPS_DocCategory adder to category list in COMPS_Doc
+ * @param doc COMPS_Doc instance
+ * @param obj COMPS_DocCategory object
+ * append COMPS_DocCategory object to category list in COMPS_Doc structure
+ * \warning function doesn't increment COMPS_DocCategory object reference count.
+ */
 HEAD_COMPS_DOC_ADDOBJLIST(category, COMPS_DocCategory) /*comps_doc.h macro*/
+
+/** COMPS_DocEnv adder to environment list in COMPS_Doc
+ * @param doc COMPS_Doc instance
+ * @param obj COMPS_DocEnv object
+ * append COMPS_DocEnv object to environment list in COMPS_Doc structure
+ * \warning function doesn't increment COMPS_DocEnv object reference count.
+ */
 HEAD_COMPS_DOC_ADDOBJLIST(environment, COMPS_DocEnv) /*comps_doc.h macro*/
 
+/** Langpack adder to langpack dict in COMPS_Doc
+ * @param doc COMPS_Doc instance
+ * @param key COMPS_Str dictionary key of langpack
+ * @param obj COMPS_Str langpack
+ * add langpack string to langpack dict in COMPS_Doc structure. If There's
+ * allready langpack string with same key, will be overwritten.
+ * \warning function doesn't increment obj param reference count.
+ */
 HEAD_COMPS_DOC_ADDOBJDICT(langpack) /*comps_doc.h macro*/
+
+/** Blacklist adder to blacklist multi-dict in COMPS_Doc
+ * @param doc COMPS_Doc instance
+ * @param key COMPS_Str dictionary key of blacklist
+ * @param obj COMPS_Str blacklist item
+ * append blacklist item object to blacklist in COMPS_Doc structure. Items 
+ * with same key are grouped in COMPS_ObjList object.
+ * \warning function doesn't increment obj param reference count.
+ */
 HEAD_COMPS_DOC_ADDOBJMDICT(blacklist) /*comps_doc.h macro*/
+
+/** whiteout adder to whitetout multi-dict in COMPS_Doc
+ * @param doc COMPS_Doc instance
+ * @param key COMPS_Str dictionary key of whiteout
+ * @param obj COMPS_Str whiteout item
+ * append whiteout item object to blacklist in COMPS_Doc structure. Items 
+ * with same key are grouped in COMPS_ObjList object.
+ * \warning function doesn't increment obj param reference count.
+ */
 HEAD_COMPS_DOC_ADDOBJMDICT(whiteout) /*comps_doc.h macro*/
+
+/**@}*/
 
 COMPS_ObjList* comps_doc_get_groups(COMPS_Doc *doc, char *id, char *name,
                                                  char *desc, char *lang);
 
+/** Parse comps.xml file into COMPS_Doc structure
+ * @param doc COMPS_Doc structure where to store
+ * @param filename opened filename
+ * @param enc file encryption
+ * @param log COMPS_Log object where log messages are stored
+ */
 void comps_doc_xml_f(COMPS_Doc* doc,  char *filename, char *enc, COMPS_Log *log);
+
+/** Parse comps from string into COMPS_Doc object
+ * @param doc COMPS_Doc structure where to store
+ * @param enc file encryption
+ * @param log COMPS_Log object where log messages are stored
+ */
 char* comps_doc_xml_str(COMPS_Doc* doc, char *enc, COMPS_Log *log);
 signed char comps_doc_xml(COMPS_Doc *doc, xmlTextWriterPtr writer);
 
