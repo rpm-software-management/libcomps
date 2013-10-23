2c56e049c2b01af0127b6b0a86ef55125b7721f5
Jindrich Luza <jluza@redhat.com>
2013-10-23 14:26:02 +0200

    - check_comps tests fixed

--- libcomps/src/comps_doc.c
+++ libcomps/src/comps_doc.c
@@ -155,9 +155,13 @@ void comps2xml_f(COMPS_Doc * doc, char *filename, char stdoutredirect) {
     doc->log->redirect2output = stdoutredirect;
     xmlTextWriterPtr writer = xmlNewTextWriterDoc(&xmldoc, 0);
     
-    str = comps_object_tostr((COMPS_Object*)doc->encoding);
-    retc = xmlTextWriterStartDocument(writer, NULL, str, NULL);
-    free(str);
+    if ((COMPS_Object*)doc->encoding) {
+        str = comps_object_tostr((COMPS_Object*)doc->encoding);
+        retc = xmlTextWriterStartDocument(writer, NULL, str, NULL);
+        free(str);
+    } else {
+        retc = xmlTextWriterStartDocument(writer, NULL, "UTF-8", NULL);
+    }
     if (retc<0)
         comps_log_error(doc->log, NULL, COMPS_ERR_XMLGEN, 0, 0, 0);
     comps_doc_xml(doc, writer, doc->log);
@@ -186,10 +190,13 @@ char* comps2xml_str(COMPS_Doc *doc) {
     xmlOutputBuffer *xmlobuff = xmlOutputBufferCreateBuffer(xmlbuff, 0);
     xmlTextWriterPtr writer = xmlNewTextWriterDoc(&xmldoc, 0);
 
-    str = comps_object_tostr((COMPS_Object*)doc->encoding);
-    retc = xmlTextWriterStartDocument(writer, NULL, str, NULL);
-    free(str);
-
+    if ((COMPS_Object*)doc->encoding) {
+        str = comps_object_tostr((COMPS_Object*)doc->encoding);
+        retc = xmlTextWriterStartDocument(writer, NULL, str, NULL);
+        free(str);
+    } else {
+        retc = xmlTextWriterStartDocument(writer, NULL, "UTF-8", NULL);
+    }
     if (retc<0) comps_log_error(doc->log, NULL, COMPS_ERR_XMLGEN, 0, 0, 0);
     comps_doc_xml(doc, writer, doc->log);
     retc = xmlTextWriterEndDocument(writer);
--- libcomps/tests/check_comps.c
+++ libcomps/tests/check_comps.c
@@ -620,24 +620,40 @@ START_TEST(test_doc_defaults) {
     COMPS_Doc * doc, *doc2;
     COMPS_Parsed *parsed;
     COMPS_ObjList *groups;
-    COMPS_Object *tmpobj;
+    COMPS_Object *tmpobj, *enc;
+    COMPS_DocGroupPackage *p;
     char *tmp;
 
     printf("### start test_comps_default\n");
-    doc = comps_object_create(&COMPS_Doc_ObjInfo, NULL);
-    g = comps_object_create(&COMPS_DocGroup_ObjInfo, NULL);
+    
+    enc = comps_str("UTF-8");
+    doc = comps_object_create(&COMPS_Doc_ObjInfo, (COMPS_Object*[]){enc});
+    COMPS_OBJECT_DESTROY(enc);
+    g = (COMPS_DocGroup*)
+        comps_object_create(&COMPS_DocGroup_ObjInfo, NULL);
     comps_docgroup_set_id(g, "group1", 0);
+
+    p = (COMPS_DocGroupPackage*)
+        comps_object_create(&COMPS_DocGroupPackage_ObjInfo, NULL);
+    comps_docpackage_set_name(p, "package1", 0);
+    comps_docgroup_add_package(g, p);
+
     comps_doc_add_group(doc, g);
     tmp = comps2xml_str(doc);
-
     parsed = comps_parse_parsed_create();
     comps_parse_parsed_init(parsed, "UTF-8", 1);
     comps_parse_str(parsed, tmp);
     doc2 = parsed->comps_doc;
     groups = comps_doc_groups(doc2);
-    g = groups->first;
-    tmpobj = comps_docgroup_uservisible(g);
+    g = groups->first->comps_obj;
+    tmpobj = comps_docgroup_get_uservisible(g);
     ck_assert(((COMPS_Num*)tmpobj)->val == 1);
+
+    COMPS_OBJECT_DESTROY(groups);
+    COMPS_OBJECT_DESTROY(tmpobj);
+    COMPS_OBJECT_DESTROY(doc);
+    free(tmp);
+    comps_parse_parsed_destroy(parsed);
 
 }END_TEST
 
@@ -650,6 +666,7 @@ Suite* basic_suite (void)
     tcase_add_test (tc_core, test_comps_doc_xml);
     tcase_add_test (tc_core, test_comps_doc_setfeats);
     tcase_add_test (tc_core, test_comps_doc_union);
+    tcase_add_test (tc_core, test_doc_defaults);
     suite_add_tcase (s, tc_core);
     return s;
 }
