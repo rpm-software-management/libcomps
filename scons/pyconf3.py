524b9f266fb0978dbd8dee5182c3fc819c0b9f38
Jindrich Luza <jluza@redhat.com>
2014-04-23 17:07:25 +0200

    - scons build almost finished

--- scons/pyconf3.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python3
-import json
-
-import distutils.sysconfig as sysconfig
-data = {"LIBS": 'python' + sysconfig.get_config_var('LDVERSION'),
-        "CPPPATH": sysconfig.get_python_inc(),
-        "LIBPATH": sysconfig.get_config_var('LIBDIR')}
-print (json.dumps(data))
