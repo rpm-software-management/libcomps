e12a0012f8087cd2384679040228321523bc4385
vil02 <65706193+vil02@users.noreply.github.com>
2026-02-23 19:28:34 +0100

    fix: use `%u` when dealing with unsigned variabnles in `printf`

--- libcomps/src/comps_radix.c
+++ libcomps/src/comps_radix.c
@@ -97,7 +97,7 @@ void comps_rtree_destroy(COMPS_RTree * rt) {
 void comps_rtree_print(COMPS_HSList * hl, unsigned  deep) {
     COMPS_HSListItem * it;
     for (it = hl->first; it != NULL; it=it->next) {
-        printf("%d %s\n",deep, (((COMPS_RTreeData*)it->data)->key));
+        printf("%u %s\n",deep, (((COMPS_RTreeData*)it->data)->key));
         comps_rtree_print(((COMPS_RTreeData*)it->data)->subnodes, deep+1);
     }
 }
